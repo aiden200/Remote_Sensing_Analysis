942d6f6bb85556681a759d99dc0afd779459e965
Aiden Chang <aidenchang@gmail.com>
2024-04-20 21:05:17 -0700

    init files

--- setup.py
+++ setup.py
@@ -6,13 +6,18 @@ setup(
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[
-        # Add your project dependencies here
+        'pillow==10.3.0'
+        'ultralytics==8.2.2',
+        'transformers==4.40.0',
+        'sentence_transformers==2.7.0',
+        'sentencepiece==0.2.0',
+        'timm==0.9.16',
     ],
-    python_requires='>=3.8',
+    python_requires='>=3.9',
     author='Aiden Chang',
     author_email='aidenchang@gmail.com',
     description='Library that conducts analysis on Satellite Imagery',
     license='MIT',
-    keywords='Machine-Learning Remote-Sensing',
+    keywords='Machine-Learning Remote-Sensing Object-detection',
     url='http://github.com/aiden200/Remote_Sensing_Analysis',
 )
